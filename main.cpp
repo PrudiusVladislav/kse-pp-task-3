dd9a3a227e677e374a25d02f4603aa32da60f71d
Vlad Prudius <vladvlpr006@gmail.com>
2024-06-15 19:06:15 +0300

    feat: add run args, copy to clipboard option

--- main.cpp
+++ main.cpp
@@ -2,7 +2,13 @@
 #include <cstring>
 #include <windows.h>
 
-typedef char*(*function_ptr)(char*);
+typedef char*(*function_ptr)(char*, int);
+
+struct Options
+{
+    bool copy_to_clipboard = false;
+    int key = 3;
+};
 
 char* getDllPath(const char *dllName)
 {
@@ -18,6 +24,24 @@ function_ptr getFunction(const HINSTANCE handle, const char *functionName)
     return reinterpret_cast<function_ptr>(GetProcAddress(handle, functionName));
 }
 
+const Options *parse_options(int argc, char *argv[])
+{
+    auto *options = new Options();
+    for (int i = 0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "-c") == 0 || strcmp(argv[i], "--copy") == 0)
+        {
+            options->copy_to_clipboard = true;
+        }
+        else if (strcmp(argv[i], "-k") == 0 || strcmp(argv[i], "--key") == 0)
+        {
+            options->key = strtol(argv[i + 1], nullptr, 10);
+        }
+    }
+
+    return options;
+}
+
 void print_help()
 {
     std::cout << "Available operations: encrypt, decrypt" << std::endl;
@@ -25,20 +49,42 @@ void print_help()
 
 char *prompt_user()
 {
-    std::cout << "Enter operation: ";
+    std::cout << "Enter operation: " << std::flush;
     char *operation_name = new char[20];
     std::cin >> operation_name;
     return operation_name;
 }
 
-const char *handle_operation(HINSTANCE handle, const char *operation_name)
+void copy_to_clipboard(const char *text)
+{
+    const size_t len = strlen(text) + 1;
+    HGLOBAL hMem = GlobalAlloc(GMEM_MOVEABLE, len);
+    memcpy(GlobalLock(hMem), text, len);
+    GlobalUnlock(hMem);
+    OpenClipboard(nullptr);
+    EmptyClipboard();
+    SetClipboardData(CF_TEXT, hMem);
+    CloseClipboard();
+    GlobalFree(hMem);
+}
+
+const char *handle_operation(
+    HINSTANCE handle,
+    const char *operation_name,
+    bool *running,
+    const Options *options)
 {
     function_ptr operation;
-    if (strcmp(operation_name, "encrypt") == 0)
+    if (strcmp(operation_name, "exit") == 0)
+    {
+        *running = false;
+        return "Exiting...";
+    }
+    if (operation_name[0] == 'e')
     {
         operation = getFunction(handle, "encrypt");
     }
-    else if (strcmp(operation_name, "decrypt") == 0)
+    else if (operation_name[0] == 'd')
     {
         operation = getFunction(handle, "decrypt");
     }
@@ -52,15 +98,23 @@ const char *handle_operation(HINSTANCE handle, const char *operation_name)
         return "Operation not found";
     }
 
-    char *message = new char[100];
+    const auto message = new char[30];
     std::cout << "Enter message: ";
-    std::cin >> message;
-    return operation(message);
+    std::cin.ignore();
+    std::cin.getline(message, 30);
+
+    const char *result = operation(message, options->key);
+    if (options->copy_to_clipboard)
+    {
+        copy_to_clipboard(result);
+    }
+
+    return result;
 }
 
 
-
-int main() {
+int main(int argc, char *argv[]) {
+    std::cout << isalpha(',') << std::endl;
     const char *dllName = "encrypt.dll";
     HINSTANCE handle = LoadLibrary(TEXT(getDllPath(dllName)));
 
@@ -70,9 +124,19 @@ int main() {
         return -1;
     }
 
+    bool running = true;
+    const Options *options = parse_options(argc, argv);
+    std::cout << "Options: copy to clipboard - " << options->copy_to_clipboard << ", key - " << options->key << std::endl;
     print_help();
-    const char *operation_name = prompt_user();
-    const char *message = handle_operation(handle, operation_name);
-    std::cout << message << std::endl;
+
+    while (running) {
+        const char *operation_name = prompt_user();
+
+        const char *message = handle_operation(handle, operation_name, &running, options);
+        std::cout << message << std::endl;
+    }
+
+    FreeLibrary(handle);
+    delete options;
     return 0;
 }
